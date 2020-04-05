d8440a1ac40a2aa114a4d6c7d28edb03e48c2771
zhanknight <zhanknight@gmail.com>
2020-04-05 17:07:00 -0400

    Major styling, minor functionality

---
+++ README.md
@@ -0,0 +1,17 @@
+# TO DO LIST
+
+This is a standard to do list app using jQuery.
+
+
+Although jQuery isn't used often in new projects these days, I wanted to write something using its most current version as an exercise. I'll write the entire thing in modern vanilla JavaScript soon.  
+
+
+## Built With
+
+* jQuery 3.4.1
+* HTML & CSS
+
+
+## Acknowledgments
+
+* Colt Steele's jQuery tutorial
--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,43 +1,58 @@
 
+// slide down the whole thing on page load for extra prettiness
 $(document).ready(function(){
     $('.flex-container').slideDown(500);
 });
 
 // make list items clickable to signal completion
+// this watches for clicks on the list item text span,
+// but is attached to the whole section so new items get it too
 $("section.theList").on("click", "span.listItemText", function() {
     $(this).toggleClass("finished");
 });
 
 // make delete button remove list items
+// slides them out for prettiness
 $("section.theList").on("click", "span.delete", function(event) {
     event.stopPropagation();
-    $(this).parent().fadeOut("200", function() {
+    $(this).parent().slideUp("200", function() {
         $(this).remove();
     });
 });
 
 // add a new item when user hits enter
+// adds whatever user has typed plus html, delete icon, etc
 $("#textInput").on("keydown", function(event) {
 if (event.which === 13) {
-    $(".theList").append("<div class=\"listItem\"><span class=\"listItemText\">" + $(this).val() + "</span><span class=\"delete\">X</span></div>")
+    $(".theList").append("<div class=\"listItem\"><span class=\"listItemText\">" + $(this).val() + "</span><span class=\"material-icons delete\">delete</span></div>")
+    $(".theList").find('div').last().hide().slideDown();
     $(this).val("");  
     } 
 })
 
 // clear all list items button
+// this slides up the whole list,
+// then empties it, 
+// then slides it out again (so new items can be added)
 $("#clearAllButton").on("click", function() {
-    $(".theList").html("");
+    $(".theList").slideUp("200", function() {
+        $(".theList").html("");
+        $(".theList").slideDown();
+    });   
 })
 
-// Dark mode / Light mode
+// Dark mode / Light mode toggle
 $("#darkMode").on("click", function() {
     $("body").toggleClass("darkMode");
 })
 
 //TODO: 
+//
 // Create Priority Flags or 'stars' for important tasks
-// Make darkmode toggle change more than just background
-//   --- can I do this with a universal class? 
-// Make it NOT UGLY!
-// Fix margins and padding
+// A bit more styling, fonts could use some work
+// Obviously needs to save it's state to be useful at all
+//
+
+
+
 
